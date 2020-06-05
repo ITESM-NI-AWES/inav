36431ee6ea5b9f3d159c977aae11f55980958f7b
garciaoyervides <garciaoyervides@gmail.com>
2020-06-04 22:53:18 -0500

    reads simulink packages like<10B>, handles 1KHz

--- src/main/custom/custom.c
+++ src/main/custom/custom.c
@@ -1,27 +1,39 @@
 #include "io/serial.h"
 #include "drivers/serial.h"
 #include "common/time.h"
+#include "common/printf.h"
 #include <stdint.h>
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAXIMUM_STRING_SIZE 50
 static serialPort_t * serialPort = NULL;
 
 void customSerialTest (timeUs_t currentTimeUs){
     UNUSED(currentTimeUs);
-    static uint8_t serialValue = 0;
-    //char buf[10] = " Hola!!\n";
-    if (serialRxBytesWaiting(serialPort)) {
-        serialValue = serialRead(serialPort);
-        if (serialValue<254){
-            serialValue++;
-        } else{
-            serialValue = 0;
+    char inBuf[MAXIMUM_STRING_SIZE];
+    char outBuf[MAXIMUM_STRING_SIZE];
+    uint8_t i = 0;
+    static double serialValue = (double)0.0;
+    bool startReading = false;
+
+    while (serialRxBytesWaiting(serialPort)) {
+        char c = serialRead(serialPort);
+        if (i >= MAXIMUM_STRING_SIZE) break;
+        if (startReading){
+            inBuf[i] = c;
+            i++;
         }
-        serialWrite(serialPort,serialValue);
-        //serialPrint(serialPort, buf);
+        if(c == '<')startReading= true;
+        if (c == '>')break;
+    }
+    if(strlen(inBuf)>9){
+        serialValue = atof(inBuf);
+        serialValue += (double)0.01;
+        tfp_sprintf(outBuf,"%10f", serialValue);
+        serialPrint(serialPort, outBuf);
     }
 }
 void customSerialTest_Init (void){
-    serialPort = openCustomSerialPort(NULL, NULL, baudRates[BAUD_115200], MODE_RX | MODE_TX, SERIAL_NOT_INVERTED | SERIAL_STOPBITS_1 | SERIAL_PARITY_NO );
-    //serialPort = openCSerialPort(portConfig->identifier, FUNCTION_NONE, NULL, NULL, baudRates[BAUD_115200], MODE_TX, SERIAL_NOT_INVERTED | SERIAL_STOPBITS_1 | SERIAL_PARITY_NO );
-    //serialPort = uartOpen(USART2, NULL, NULL, baudRates[BAUD_115200], MODE_TX, SERIAL_NOT_INVERTED | SERIAL_STOPBITS_1 | SERIAL_PARITY_NO);
+    serialPort = openCustomSerialPort(NULL, NULL, baudRates[BAUD_921600], MODE_RX | MODE_TX, SERIAL_NOT_INVERTED | SERIAL_STOPBITS_1);
 }
